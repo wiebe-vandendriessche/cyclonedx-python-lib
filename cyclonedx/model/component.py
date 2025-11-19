f21c92b376be92f3ba8b390a4ef5ca54864f922f
Wiebe Vandendriessche <wiebe.vandendriessche@gmail.com>
2025-11-19 13:18:05 +0100

    feat: add ComponentData model and related tests
    Signed-off-by: Wiebe Vandendriessche <wiebe.vandendriessche@gmail.com>

--- cyclonedx/model/component.py
+++ cyclonedx/model/component.py
@@ -70,6 +70,7 @@ from .dependency import Dependable
 from .issue import IssueType
 from .license import License, LicenseRepository, _LicenseRepositorySerializationHelper
 from .model_card import ModelCard
+from .data import ComponentData
 from .release_note import ReleaseNotes
 
 
@@ -1014,6 +1015,7 @@ class Component(Dependable):
         omnibor_ids: Optional[Iterable[OmniborId]] = None,
         swhids: Optional[Iterable[Swhid]] = None,
         crypto_properties: Optional[CryptoProperties] = None,
+        data: Optional[Iterable[ComponentData]] = None,
         tags: Optional[Iterable[str]] = None,
         # Deprecated in v1.6
         author: Optional[str] = None,
@@ -1046,6 +1048,7 @@ class Component(Dependable):
         self.release_notes = release_notes
         self.model_card = model_card
         self.crypto_properties = crypto_properties
+        self.data = data or []
         self.tags = tags or []
         # spec-deprecated properties below
         self.author = author
@@ -1656,16 +1659,24 @@ class Component(Dependable):
     # def model_card(self, ...) -> None:
     #     ...  # TODO since CDX1.5
 
-    # @property
-    # ...
-    # @serializable.view(SchemaVersion1Dot5)
-    # @serializable.xml_sequence(23)
-    # def data(self) -> ...:
-    #     ...  # TODO since CDX1.5
-    #
-    # @data.setter
-    # def data(self, ...) -> None:
-    #     ...  # TODO since CDX1.5
+    @property
+    @serializable.view(SchemaVersion1Dot5)
+    @serializable.view(SchemaVersion1Dot6)
+    @serializable.view(SchemaVersion1Dot7)
+    @serializable.xml_array(serializable.XmlArraySerializationType.FLAT, 'data')
+    @serializable.xml_sequence(27)
+    def data(self) -> 'SortedSet[ComponentData]':
+        """
+        Specifies data objects for components of type `data`.
+
+        Returns:
+            `Iterable[ComponentData]`
+        """
+        return self._data
+
+    @data.setter
+    def data(self, data: Iterable[ComponentData]) -> None:
+        self._data = SortedSet(data)
 
     @property
     @serializable.view(SchemaVersion1Dot6)
@@ -1737,7 +1748,8 @@ class Component(Dependable):
             _ComparableTuple(self.external_references), _ComparableTuple(self.properties),
             _ComparableTuple(self.components), self.evidence, self.release_notes, self.modified,
             _ComparableTuple(self.authors), _ComparableTuple(self.omnibor_ids), self.manufacturer,
-            self.crypto_properties, _ComparableTuple(self.tags), self.model_card, self.is_external,
+            _ComparableTuple(self.data), self.crypto_properties, _ComparableTuple(
+                self.tags), self.model_card, self.is_external,
         ))
 
     def __eq__(self, other: object) -> bool:
